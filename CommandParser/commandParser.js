5b70b2366e4c4daf856638f464c73421421792ac
Sviat Oleksyn <sviat1001@gmail.com>
2023-06-06 21:11:47 +0300

    Updated commandParser.js

--- CommandParser/commandParser.js
+++ CommandParser/commandParser.js
@@ -9,6 +9,33 @@ class CommandParser {
     constructor(commands) {
         this.#commands = commands;
     }
+    userCommand(command) {
+        switch(command){
+            case 'list': 
+                getTasksList()
+                break;
+            case 'late': 
+                getLateTasks()
+                break;
+            case 'create': 
+                CreateTask(title, description = "", deadline = "")
+                break;
+            case 'show': 
+                ShowTask(id)
+                break;
+            case 'udpate': 
+                UpdateTask(id, title = "", description = "", deadline = "")  
+                break;
+            case 'delete':
+                DeleteTask(id) 
+                break; 
+            case 'complete': 
+                DeleteTask(id) 
+                break;
+            default:
+                throw new Error("Command does not exist");
+        }
+    }
 }
 
 module.exports = CommandParser;
