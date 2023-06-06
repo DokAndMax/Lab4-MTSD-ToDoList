bb88bc267a2b75c4b13f2ae688c210536cca77a2
Maks <maxim.kryvenok@gmail.com>
2023-06-06 19:25:36 +0300

    Init project infrastructure

---
+++ AppLogic/Interfaces/IDataBase.js
@@ -0,0 +1,17 @@
+export default class IDataBase {
+    Create(task)  {
+        throw new Error("Method not implemented.");
+    }
+    Read(task)  {
+        throw new Error("Method not implemented.");
+    }
+    Update(task)  {
+        throw new Error("Method not implemented.");
+    }
+    Delete(task)  {
+        throw new Error("Method not implemented.");
+    }
+    Search(task)  {
+        throw new Error("Method not implemented.");
+    }
+}
